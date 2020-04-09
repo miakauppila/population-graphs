0e0f60042cada0acbbbe53fb27fba13de5798da5
miakauppila <mia.kauppila@gmail.com>
2020-04-10 01:20:17 +0300

    Added Bootsrap styles + clearChart.

--- myChart.js
+++ myChart.js
@@ -1,14 +1,23 @@
+let currentChart;
+
 document.getElementById('renderBtn').addEventListener('click', checkCountryCode);
-//Empty the canvas when clicked?
 
 function checkCountryCode() {
+    clearChart();
     let countryInput = document.getElementById('country');
-    let canvas = document.getElementById("myChart");
     if (!countryInput.checkValidity()) {
         document.getElementById("error").innerHTML = "Please check the country code format. Valid example: USA";
-    } else {
+    }
+    else {
         document.getElementById("error").innerHTML = "";
         fetchData();
+    }
+}
+
+function clearChart() {
+    if (currentChart) {
+        // Clear the previous chart if it exists kts. Chart.js API
+        currentChart.destroy();
     }
 }
 
@@ -46,26 +55,25 @@ async function fetchData() {
         console.log(err);
         renderError('Population data could not be retrieved. ' + err);
     }
-
 }
 
 function getValues(data) {
-    var values = data[1].sort((a, b) => a.date - b.date).map(item => item.value);
+    let values = data[1].sort((a, b) => a.date - b.date).map(item => item.value);
     return values;
 }
 
 function getLabels(data) {
-    var labels = data[1].sort((a, b) => a.date - b.date).map(item => item.date);
+    let labels = data[1].sort((a, b) => a.date - b.date).map(item => item.date);
     return labels;
 }
 
 function getCountryName(data) {
-    var countryName = data[1][0].country.value;
+    let countryName = data[1][0].country.value;
     return countryName;
 }
 
 function getIndicatorName(data) {
-    var indicatorName = data[1][0].indicator.value;
+    let indicatorName = data[1][0].indicator.value;
     return indicatorName;
 }
 
@@ -73,15 +81,9 @@ function renderError(message) {
     document.getElementById("error").innerHTML = message;
 }
 
-var currentChart;
-
 function renderChart(data, labels, countryName, indicatorName) {
-    var ctx = document.getElementById('myChart').getContext('2d');
+    let ctx = document.getElementById('myChart').getContext('2d');
 
-    if (currentChart) {
-        // Clear the previous chart if it exists kts. Chart.js API
-        currentChart.destroy();
-    }
     // Draw new chart
     currentChart = new Chart(ctx, {
         type: 'line',
